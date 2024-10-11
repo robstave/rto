49c77ca5b7d63ee7fe8e32b92e257a2c06633935
robstave <robstave@yahoo.com>
2024-10-11 18:51:15 -0500

    add target to d3 graph

--- internal/adapters/controller/chart.go
+++ internal/adapters/controller/chart.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -9,10 +10,23 @@ import (
 	"github.com/robstave/rto/internal/utils"
 )
 
+// ChartResponse represents the JSON response for the chart data
+type ChartResponse struct {
+	Data       []map[string]interface{} `json:"data"`
+	TargetDays float64                  `json:"targetDays"`
+}
+
 // GetChartData handles the retrieval of data for the D3 chart
 func (ctlr *RTOController) GetChartData(c echo.Context) error {
 	// Fetch all events from the service
 	events := ctlr.service.GetAllEvents()
+	prefs := ctlr.service.GetPrefs()
+	targetDaysStr := prefs.TargetDays
+	targetDays, err := strconv.ParseFloat(targetDaysStr, 64)
+	if err != nil {
+		// Fallback to default target if parsing fails
+		targetDays = 2.5
+	}
 
 	// Build the date range (October 1, 2024 to December 31, 2024)
 	startDate := time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC)
@@ -39,8 +53,15 @@ func (ctlr *RTOController) GetChartData(c echo.Context) error {
 		})
 	}
 
-	// Return the data as JSON
-	return c.JSON(http.StatusOK, data)
+	// Create the ChartResponse
+	response := ChartResponse{
+		Data:       data,
+		TargetDays: targetDays,
+	}
+
+	// Return the ChartResponse as JSON
+	return c.JSON(http.StatusOK, response)
+
 }
 
 // Utility function to count attendance on a specific date
